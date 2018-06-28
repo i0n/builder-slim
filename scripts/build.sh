1feb2aa83dc2dda0aa645c1d2854caf18742834e
i0n <ianalexanderwood@gmail.com>
2018-06-28 21:15:41 +0100

    first commit

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+current_dir=`pwd`
+script_rel_dir=`dirname $0`
+script_dir=`cd $script_rel_dir && pwd`
+root_dir=`dirname $script_dir`
+
+docker build -t ${DOCKER_HUB_USER}/builder-slim:$(./scripts/get-version-number-from-repo.sh | tail -1) .
+docker build -t ${DOCKER_HUB_USER}/builder-slim:latest .
